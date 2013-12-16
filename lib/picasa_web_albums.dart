a9989e4bd17476490d5065be9e140da2a2206d0c
hangstrap <hangstrap@gmail.com>
2013-12-16 22:10:40 +1300

    wip

--- lib/picasa_web_albums.dart
+++ lib/picasa_web_albums.dart
@@ -1,14 +1,16 @@
 library picasa_web_album;
+
 import "dart:io";
+import "dart:async";
 import 'package:json_object/json_object.dart';
+import 'package:http/http.dart' as http;
 
 class Photo{
   JsonObject json;
   Photo( this.json);
-
-  factory Photo.fromJsonString(String jsonStr) {    
-    return new Photo( new JsonObject.fromJsonString( jsonStr, new JsonObject()));
-  }
+  
+  String get title => json.title.$t;
+  String get url  => json.media$group.media$content[0].url;   
 
 }
 
@@ -22,21 +24,36 @@ class Album {
   String get rights => json.rights.$t;
   
   List<Photo> get photos => json.photos;
-  
-  factory Album.fromJsonString(String jsonStr) {    
-    return new Album( new JsonObject.fromJsonString( jsonStr, new JsonObject()));
-  }
+    
   
   String getAlumUri(){
     List<JsonObject> links = json.link;
     JsonObject link = links.firstWhere( (JsonObject e)=> e.rel.startsWith( "http://schemas"));
     return link.href;
   }
+
+
 }
 
-class UsersAlbums{
+  
+class User{
+  
+  String id;
+  User( this.id);
+      
+  List<Album> loadFromJson( JsonObject json){       
+    List<JsonObject> entries = json.feed.entry;
+    List<Album> result = [];
+    entries.forEach( (e)=> result.add( new Album( e)));
+    return result;
+  }
 
-  List<Album> albums(){
-    HttpClient client = new HttpClient();
+
+  Future<List<Album>> albums(){
+    String myAlbum = "https://picasaweb.google.com/data/feed/api/user/${id}?alt=json";
+    return http.get( myAlbum ).then( (response){
+      JsonObject json = new JsonObject.fromJsonString( response.body);
+      return loadFromJson( json);
+    });    
   }
 }
--- test/picasa_web_albums_test.dart
+++ test/picasa_web_albums_test.dart
@@ -1,19 +1,68 @@
-import '../lib/picasa_web_albums.dart';
+import 'package:json_object/json_object.dart';
+import '../lib/picasa_web_albums.dart' ;
 import 'package:unittest/unittest.dart';
 import 'dart:io';
+import 'dart:async';
 
 void main(){
+  group( "When loading from precanned json data", (){
+    
+    test( "should load album from json",() {
+      Album album = new Album( getJsonForAlbum());
+      expect( album.title, equals( "Tessa d\'Jappervilla"));
+      expect( album.rights, equals( "public"));
+      expect( album.getAlumUri(), equals( "https://picasaweb.google.com/data/feed/api/user/101488109748928583216/albumid/5938894451891583841?alt=json"));
+    });
+    
+    test( "should load photo from json", (){
+      Photo photo = new Photo( getJsonForPhoto());
+      expect( photo.title, equals( "2013-10-26 09.36.22.jpg"));
+      expect( photo.url, equals( "https://lh4.googleusercontent.com/-GGLasOTPgxk/UmsxCvL-zfI/AAAAAAAATno/wP-1Ql82kIs/2013-10-26%25252009.36.22.jpg"));
+    });
+    
+    test("should load albums from json object", (){        
+      List<Album> albums = new User("aa").loadFromJson( getJsonForUser());
+      expect( albums.length, equals( 85));
+      expect( albums.first.title, equals( "Tessa d\'Jappervilla"));
+      expect( albums.last.title, equals( "Mana Island"));
+    });
+  });  
   
-  test( "Load album from json",() {
-    Album album = new Album.fromJsonString( getJsonForAlbum());
-    expect( album.title, equals( "Tessa d\'Jappervilla"));
-    expect( album.rights, equals( "public"));
-    expect( album.getAlumUri(), equals( "https://picasaweb.google.com/data/feed/api/user/101488109748928583216/albumid/5938894451891583841?alt=json"));
+  group( "When loading from actual web service", (){
+    
+    User user ;
+    setUp((){
+      user = new User( "101488109748928583216");
+    });
+    
+    test( "should return at least the current number of my albums", (){
+      User user = new User( "101488109748928583216");
+      Future< List<Album>> albumsFuture = user.albums();
+      expect( albumsFuture.then( (albums)=> albums.length), completion( greaterThanOrEqualTo( 85)));      
+    });
+    test( "should return my first album correctly", (){
+      User user = new User( "101488109748928583216");
+      Future< List<Album>> albumsFuture = user.albums();
+      expect( albumsFuture.then( (albums)=> albums.first.title), completion( equals( 'Tessa d\'Jappervilla')));      
+    });
+    test("", (){
+      user.albums().then( (albums) => albums.forEach( (album)=>print( "${album.title} ${album.rights}")));
+    });
   });
+  
+  
+}
+
+JsonObject getJsonForPhoto() {
+  return new JsonObject.fromJsonString( new File(  "photo.json").readAsStringSync());
 }
 
 
-String getJsonForAlbum(){
-  return new File(  "album.json").readAsStringSync();  
+JsonObject getJsonForAlbum(){
+  return new JsonObject.fromJsonString( new File(  "album.json").readAsStringSync());  
 }
 
+
+JsonObject getJsonForUser(){
+  return new JsonObject.fromJsonString( new File(  "user.json").readAsStringSync(), new JsonObject())  ;
+}
